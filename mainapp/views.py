26e4d32b62aa7a2c03a8612ef74bb69454440fdd
habinezajanvier <habinezajanvier@gmail.com>
2022-02-03 16:45:19 +0200

    fix(error): catch errors

--- mainapp/views.py
+++ mainapp/views.py
@@ -10,15 +10,18 @@ from rest_framework.response import Response
 
 @api_view(["GET"])
 def getWeather(request, name):
+    try:
+        appId = "3c532881afa6630ff2a29d928a7246f2"
+        URLS = "https://api.openweathermap.org/data/2.5/weather"
+        PARAMS = {"q": name, "appId": appId, "units": "metric"}
 
-    appId = "3c532881afa6630ff2a29d928a7246f2"
-    URLS = "https://api.openweathermap.org/data/2.5/weather"
-    PARAMS = {"q": name, "appId": appId, "units": "metric"}
-
-    r = requests.get(url=URLS, params=PARAMS)
-    data = r.json()
-    responseJson = {
-        "message": "Data fetched successfully",
-        "data": data
-    }
-    return Response(responseJson, status=status.HTTP_200_OK)
+        r = requests.get(url=URLS, params=PARAMS)
+        data = r.json()
+        responseJson = {
+            "message": "Data fetched successfully",
+            "data": data
+        }
+        return Response(responseJson, status=status.HTTP_200_OK)
+    except:
+        serverError = {"error": "Something went wrong, try again latter"}
+        return Response(serverError, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
